686ecf6f176eede3cce293bb4ebc5ab50ef9fdbb
eoinoconn <eoinoconn@gmail.com>
2023-05-13 21:14:27 +0700

    add config file to project

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,7 +1,8 @@
 import unittest
 import tempfile
+import os
 
-from char.character.utils import get_char_save_files, get_char_save_file_name, check_if_char_exists
+from char.character.utils import get_char_save_files, get_char_save_file_name, check_if_char_exists, create_config_file
 from utils import setup_char_json
 
 
@@ -27,3 +28,8 @@ class TestCharacterUtils(unittest.TestCase):
             setup_char_json(temp_dir, {"name": "test"})
             self.assertRaises(ValueError, check_if_char_exists,
                               temp_dir + "/test.json")
+
+    def test_create_config_file(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            create_config_file(temp_dir, "config/default_config.yaml")
+            self.assertTrue(os.path.isfile(temp_dir + "/default_config.yaml"))
